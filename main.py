99a471cde136259024aa0f0220efec5e8f01160c
Danilo <daconnas.dcn@gmail.com>
2020-11-18 00:11:23 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.utils import get_color_from_hex
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+
+try: from .tela_home import TelaHome
+except: from tela_home import TelaHome
+
+
+class GerenciadorTelas(ScreenManager):
+    pass
+
+
+class TesteApp(App):
+    
+    kv_file = "kv_file.kv"
+    def build(self):
+        gerenciador = GerenciadorTelas()
+        return gerenciador
+
+    def on_start(self):
+        home = TelaHome()
+        screen = Screen(name='home')
+        screen.add_widget(home)
+        self.root.add_widget(screen)
+        self.root.ids.update(home.ids)
+
+        self.root.current = "home"
+        print(self.root.ids)
+        self.root.ids["home_botao_sair"].bind(on_release=self.home_sair)
+
+    def home_sair(self, botao):
+        box = BoxLayout(orientation="vertical", padding="10sp", spacing="10sp")
+        botoes = BoxLayout(padding="10sp", spacing="10sp")
+
+        pop = Popup(title="Quer mesmo sair?", content=box, title_size="25sp", size_hint=(.35, .35))
+        pop.opacity = .85
+        imagem = Image(source="./imagens/atencao.png")
+
+        sim = Button(text="sim", font_size="20sp", on_release=App.get_running_app().stop)
+        nao = Button(text="nao", font_size="20sp", on_release=pop.dismiss)
+        botoes.add_widget(sim)
+        botoes.add_widget(nao)
+
+        box.add_widget(imagem)
+        box.add_widget(botoes)
+        pop.open()
+
+
+if __name__ == "__main__":
+
+    from kivy.core.window import Window
+    Window.clearcolor = get_color_from_hex("#4C6B8A")
+    TesteApp().run()
---
+++ tela_home.py
@@ -0,0 +1,58 @@
+from kivy.app import App
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scrollview import ScrollView
+from kivy.uix.actionbar import ActionBar, ActionView, ActionPrevious, ActionButton, ActionLabel, ActionSeparator
+from kivy.graphics import Color, Rectangle
+from kivy.utils import get_color_from_hex
+from kivy.metrics import sp
+
+import os, random
+
+class TelaHome(BoxLayout):
+    
+    def __init__(self, **kwargs):
+        super(TelaHome, self).__init__(**kwargs)
+        self.orientation = "vertical"
+        self.KEY = 0
+        self.gera_imagem()
+        self.gera_botoes(.3)
+
+    def botao(self, botao):
+        print("Clicando ", botao.text, botao)
+
+        dirs = os.listdir("./imagens/home")
+        fonte = random.choice(dirs)
+        fonte = "./imagens/home/" + fonte
+
+        self.ids["imagem_home"].source = fonte
+
+    def gera_imagem(self):
+        box = BoxLayout(padding='10sp', spacing="20sp")
+        dirs = os.listdir("./imagens/home")
+        fonte = random.choice(dirs)
+        fonte = "./imagens/home/" + fonte
+        imagem = Image(source=fonte)
+        box.add_widget(imagem)
+        self.ids["imagem_home"] = imagem
+        self.add_widget(box)
+
+    def gera_botoes(self, h_rint=1):
+        p = [f"{i}sp" for i in [40, 40, 40, 40]]
+        box = BoxLayout(orientation="horizontal", padding=p, spacing="60sp")
+        box.size_hint_y = h_rint
+        
+        bnt1 = Button(text="Calculos", on_release=self.botao)
+
+        bnt2 = Button(text="Sair")
+
+        box.add_widget(bnt1)
+        box.add_widget(bnt2)
+        self.add_widget(box)
+        self.ids["home_botao_calculos"] = bnt1
+        self.ids["home_botao_sair"] = bnt2
+        
